409bdc002bbcc134b2dbf39ca61d4a1a2268cd3c
abscondment <brendan.ribera@gmail.com>
2012-06-26 09:58:01 -0700

    Stuff

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject speling "0.5.0-SNAPSHOT"
+  :description "Got speling?"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [mysql/mysql-connector-java "5.1.20"]])
---
+++ test/speling/core_test.clj
@@ -0,0 +1,6 @@
+(ns speling.core-test
+  (:use [speling.core] :reload-all)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false))
