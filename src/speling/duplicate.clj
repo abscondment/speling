3e37bb74844c72d8b4ae6f0e4131f140abb6f554
abscondment <brendan.ribera@gmail.com>
2012-06-28 02:37:42 -0700

    Bunch of duplicate detection noodling.

--- src/speling/duplicate.clj
+++ src/speling/duplicate.clj
@@ -2,6 +2,28 @@
   (:use [speling core])
   (:require [clojure.java.jdbc :as sql]))
 
+(def db {:subprotocol "mysql"
+         :subname "//127.0.0.1:3306/paperkarma_development"
+         :user "root"
+         :password nil})
+
+(defn- select-name-map [query-coll]
+  (sql/with-connection db
+    (sql/with-query-results rows
+      query-coll
+      (reduce
+       (fn [m row] (assoc m (:id row) (:name row)))
+       {} rows))))
+
+(defn new-names-map []
+  (select-name-map ["SELECT m.id, m.name FROM (SELECT mailer_id FROM unsubscribes GROUP BY mailer_id HAVING count(*) < 3 AND count(*) > 0) u INNER JOIN mailers m ON m.id = u.mailer_id AND flags & 32 = 0"]))
+
+(defn names-map []
+  (select-name-map ["SELECT id, name FROM mailers WHERE flags & 32 = 0"]))
+
+(defn names-small-map []
+  (select-name-map ["SELECT id, name FROM mailers WHERE flags & 32 = 0 LIMIT 1000"]))
+
 (defn ngrams
   ([text] (ngrams text 5))
   ([text max-n] (ngrams text 2 max-n))
@@ -11,6 +33,36 @@
               (for [n (range min-n (inc max-n))]
                 (partition n 1 %)))
       (words text))))
+
+(defn name-frequencies [name-map]
+  (persistent!
+   (reduce
+    (fn [fmap [id name]]
+      (reduce (fn [m n] (assoc! m n (conj (get m n #{}) id)))
+              fmap
+              (ngrams name)))
+    (transient {})
+    name-map)))
+
+;; TODO: can this be made faster with transients?
+(defn co-occurring-ids-map [name-map]
+  (reduce
+   (fn [imap [ngram ids]]
+     (apply merge-with
+            (fn [list-a list-b] (merge-with + list-a list-b))
+            imap
+            (map (fn [id] {id (zipmap (disj ids id) (repeat 1)) }) ids)))
+   {}
+   (name-frequencies name-map)))
+
+(comment
+ (time (pprint
+        (partition 2
+                   (apply concat
+                          (take 20
+                                (sort-by (comp count last)
+                                         (filter #(> (-> % (last) (count)) 4)
+                                                 (name-frequencies (names-map))))))))))
 
 (defn make-freqs [coll]
   (reduce
@@ -51,22 +103,6 @@
                      (Math/abs (- ad bd)))))
          (quot (+ (count freqs-a) (count freqs-b)) 2))))))
 
-(def db {:subprotocol "mysql"
-         :subname "//127.0.0.1:3306/paperkarma_development"
-         :user "root"
-         :password nil})
-
-(def new-names
-  (sql/with-connection db
-    (sql/with-query-results rows
-      ["SELECT m.name FROM (SELECT mailer_id FROM unsubscribes GROUP BY mailer_id HAVING count(*) < 3 AND count(*) > 0) u INNER JOIN mailers m ON m.id = u.mailer_id AND flags & 32 = 0"]
-      (doall (map :name rows)))))
-
-(def names
-  (sql/with-connection db
-    (sql/with-query-results rows
-      ["SELECT name FROM mailers WHERE flags & 32 = 0"]
-      (doall (map :name rows)))))
 
 (comment
   (let [all-ngrams
