c86501e0fb3ca16d5f215c19445779553ea65410
Brendan Ribera <brendan.ribera@gmail.com>
2012-07-02 15:26:44 -0700

    Here are some tests for the duplicate detection namespace.

--- test/speling/duplicate_test.clj
+++ test/speling/duplicate_test.clj
@@ -2,20 +2,43 @@
   (:use [speling.duplicate] :reload-all)
   (:use [clojure.test]
         [clojure.pprint]))
-(comment
- (def name-map
-   {1 "Pennsylvania"
-    2 "Penn's Woods"
-    3 "Carter Subaru"
-    4 "Carter Volkswadgon"
-    5 "Carter Subaru/Volkswagon"
-    6 "Carter Volkswagon"
-    7 "Penn and Teller"}
-   ))
 
-(deftest make-co
+(defn- co-map-without-counts [co-map]
+  (sort-by first
+   (map (fn [[k v]] [k (sort (map first v))])
+        co-map)))
+
+(deftest simple-matches
   (let [name-map {1 "Pennsylvania"
                   2 "Penn's Woods"
                   3 "Carter Subaru"}
-        co (co-occurring-ids-map name-map 1)]
-    (is (= co {1 {2 6} 2 {1 6}}))))
+        matches (co-occurring-ids-map name-map 1)]
+    ;; Assert that the 1st two are matches, but the third has no matches.
+    (is (= (co-map-without-counts matches)
+           [[1 [2]] [2 [1]]]))))
+
+(deftest complex-matches
+  (let [name-map {1 "Pennsylvania"
+                  2 "Penn's Woods"
+                  3 "Carter Subaru"
+                  4 "Carter Volkswadgon"
+                  5 "Carter Subaru/Volkswagon"
+                  6 "Carter Volkswagon"
+                  7 "Penn and Carter"}
+        matches (co-occurring-ids-map name-map 1)]
+    ;;
+    (is (= (co-map-without-counts matches)
+           [[1 [2 7]]
+            [2 [1 7]]
+            [3 [4 5 6 7]]
+            [4 [3 5 6 7]]
+            [5 [3 4 6 7]]
+            [6 [3 4 5 7]]
+            [7 [1 2 3 4 5 6]]]))))
+
+(deftest quality-of-matches
+  (let [name-map {1 "Brendan Ribera"
+                  2 "Brandon Ribera"
+                  3 "Brenden Rebera"
+                  4 "Jessica Ribera"
+                  6 "Jessica Marie Ribera"}]))
