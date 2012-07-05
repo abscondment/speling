4775e8091d2a31876fd6488155f0b916fd47a0ad
Brendan Ribera <brendan.ribera@gmail.com>
2012-07-05 14:42:38 -0700

    Some minor reorganization.

--- src/speling/core.clj
+++ src/speling/core.clj
@@ -1,10 +1,27 @@
 (ns speling.core
   (:require [clojure.string :as str]))
 
+(defn real-pmap [f s] (map deref (doall (map #(future (f %)) s))))
+
+
+(defn delete-if [m pred]
+  (select-keys m
+   (for [[k v :as p] m :when (pred p)] k)))
+
+
 (defn words [text]
-  (->
-   text
-   (str/lower-case)
-   (str/replace #"[']+" "'")
-   (str/replace #"[\-]+" "-")
-   (str/split #"[^a-z0-9'\-]+([^a-z0-9][^a-z0-9'\-]?)*")))
+  (-> text
+      (str/lower-case)
+      (str/replace #"[']+" "'")
+      (str/replace #"[\-]+" "-")
+      (str/split #"[^a-z0-9'\-]+([^a-z0-9][^a-z0-9'\-]?)*")))
+
+(defn ngrams
+  ([text] (ngrams text 5))
+  ([text max-n] (ngrams text 2 max-n))
+  ([text min-n max-n]
+     (mapcat
+      #(apply concat
+              (for [n (range min-n (inc max-n))]
+                (partition n 1 %)))
+      (words text))))
--- src/speling/duplicate.clj
+++ src/speling/duplicate.clj
@@ -17,40 +17,28 @@
        (fn [m row] (assoc m (:id row) (:name row)))
        {} rows))))
 
-(defn new-names-map []
+(defn- new-names-map []
   (select-name-map ["SELECT m.id, m.name FROM (SELECT mailer_id FROM unsubscribes GROUP BY mailer_id HAVING count(*) < 3 AND count(*) > 0) u INNER JOIN mailers m ON m.id = u.mailer_id AND flags & 32 = 0"]))
 
-(defn names-map []
+(defn- names-map []
   (select-name-map ["SELECT id, name FROM mailers WHERE flags & 32 = 0"]))
 
-(defn names-map-sized [limit]
+(defn- names-map-sized [limit]
   (select-name-map ["SELECT id, name FROM mailers WHERE flags & 32 = 0 LIMIT ?" limit]))
 
-(defn ngrams
-  ([text] (ngrams text 5))
-  ([text max-n] (ngrams text 2 max-n))
-  ([text min-n max-n]
-     (mapcat
-      #(apply concat
-              (for [n (range min-n (inc max-n))]
-                (partition n 1 %)))
-      (words text))))
+(defn- name-frequencies [name-map opts]
+  (let [min-ngram (get opts :min-ngram 3)
+        max-ngram (get opts :max-ngram 7)]
+    (persistent!
+     (reduce
+      (fn [fmap [id name]]
+        (reduce (fn [m n] (assoc! m n (conj (get m n #{}) id)))
+                fmap
+                (ngrams name min-ngram max-ngram)))
+      (transient {})
+      name-map))))
 
-(defn name-frequencies [name-map]
-  (persistent!
-   (reduce
-    (fn [fmap [id name]]
-      (reduce (fn [m n] (assoc! m n (conj (get m n #{}) id)))
-              fmap
-              (ngrams name 3 7)))
-    (transient {})
-    name-map)))
-
-(defn- delete-if [m pred]
-  (select-keys m
-               (for [[k v :as p] m :when (pred p)] k)))
-
-(defn filtered-name-frequencies
+(defn- filtered-name-frequencies
   ([name-freqs] (filtered-name-frequencies name-freqs 1))
   ([name-freqs min-limit]
      (delete-if name-freqs (fn [[k v]] (> (count v) min-limit)))))
@@ -65,35 +53,37 @@
            freq-filter-level  (get opts :freq-filter-level 3)
            idfn (comp nmap first)
            fnmap (-> nmap
-                     (name-frequencies)
+                     (name-frequencies opts)
                      (filtered-name-frequencies freq-filter-level))]
        (doall
         (pmap
-         #(doseq [[id name] %]
-            (let [name-weight (count name)
-                  matches (->> (ngrams name)
-                               (map
-                                (fn [ngram]
-                                  (for [id (fnmap ngram)]
-                                    [id (count ngram)])))
-                               (filter not-empty)
-                               (apply concat)
-                               (sort-by first)
-                               (partition-by first)
-                               (map
-                                (fn [pairs]
-                                  [(ffirst pairs)
-                                   ;; TODO:
-                                   ;; Right now, we're scaling ngram *size* by the name size.
-                                   ;;
-                                   ;; We should also test using TF*IDF for the given ngram.
-                                   (->> pairs (map last) (reduce +) ((fn [w] (/ w name-weight))))]))
-                               (filter
-                                (fn [[id weight]]
-                                  (> weight weight-filter-level))))]
-              (f id name matches)))
+         (fn [groups]
+           (doseq [[id name] groups]
+             (let [name-weight (count name)
+                   matches (->> (ngrams name)
+                                (map
+                                 (fn [ngram]
+                                   (for [id (fnmap ngram)]
+                                     [id (count ngram)])))
+                                (filter not-empty)
+                                (apply concat)
+                                (sort-by first)
+                                (partition-by first)
+                                (map
+                                 (fn [pairs]
+                                   [(ffirst pairs)
+                                    ;; TODO:
+                                    ;; Right now, we're scaling ngram *size* by the name size.
+                                    ;;
+                                    ;; We should also test using TF*IDF for the given ngram.
+                                    (->> pairs (map last) (reduce +) ((fn [w] (/ w name-weight))))]))
+                                (filter
+                                 (fn [[id weight]]
+                                   (> weight weight-filter-level))))]
+               (f id name matches))))
          (partition-all n-threads nmap)))
        nil)))
+
 
 (defn -main []
   (do
