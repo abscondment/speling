44689e23a4d92d07f5fab2362a8643da2c48e8a8
Brendan Ribera <brendan.ribera@gmail.com>
2012-07-05 11:36:47 -0700

    Whitespace.

--- src/speling/duplicate.clj
+++ src/speling/duplicate.clj
@@ -17,14 +17,14 @@
        (fn [m row] (assoc m (:id row) (:name row)))
        {} rows))))
 
- (defn new-names-map []
-   (select-name-map ["SELECT m.id, m.name FROM (SELECT mailer_id FROM unsubscribes GROUP BY mailer_id HAVING count(*) < 3 AND count(*) > 0) u INNER JOIN mailers m ON m.id = u.mailer_id AND flags & 32 = 0"]))
+(defn new-names-map []
+  (select-name-map ["SELECT m.id, m.name FROM (SELECT mailer_id FROM unsubscribes GROUP BY mailer_id HAVING count(*) < 3 AND count(*) > 0) u INNER JOIN mailers m ON m.id = u.mailer_id AND flags & 32 = 0"]))
 
- (defn names-map []
-   (select-name-map ["SELECT id, name FROM mailers WHERE flags & 32 = 0"]))
+(defn names-map []
+  (select-name-map ["SELECT id, name FROM mailers WHERE flags & 32 = 0"]))
 
- (defn names-map-sized [limit]
-   (select-name-map ["SELECT id, name FROM mailers WHERE flags & 32 = 0 LIMIT ?" limit]))
+(defn names-map-sized [limit]
+  (select-name-map ["SELECT id, name FROM mailers WHERE flags & 32 = 0 LIMIT ?" limit]))
 
 (defn ngrams
   ([text] (ngrams text 5))
@@ -48,7 +48,7 @@
 
 (defn- delete-if [m pred]
   (select-keys m
-   (for [[k v :as p] m :when (pred p)] k)))
+               (for [[k v :as p] m :when (pred p)] k)))
 
 (defn filtered-name-frequencies
   ([name-freqs] (filtered-name-frequencies name-freqs 1))
@@ -62,46 +62,46 @@
            freq-filter-level  (get opts :freq-filter-level 3)
            idfn (comp nmap first)
            fnmap (-> nmap
-                   (name-frequencies)
-                   (filtered-name-frequencies freq-filter-level))]
-     (doall
-      (pmap
-       #(doseq [[id name] %]
-          ;;
-          ;; TODO: weigh each match by the ngram length
-          ;;
-          (let [matches (->> (ngrams name)
-                             (map fnmap)
-                             (filter identity)
-                             (apply concat)
-                             (sort)
-                             (partition-by identity)
-                             (map (fn [s] (if (> (count s) count-filter-level) [(first s) (count s)])))
-                             (filter identity)
-                             (sort-by last)
-                             (reverse))]
-            (f id name matches)))
-       (partition-all (/ (count nmap) (inc (.availableProcessors (Runtime/getRuntime)))) nmap))))))
+                     (name-frequencies)
+                     (filtered-name-frequencies freq-filter-level))]
+       (doall
+        (pmap
+         #(doseq [[id name] %]
+            ;;
+            ;; TODO: weigh each match by the ngram length
+            ;;
+            (let [matches (->> (ngrams name)
+                               (map fnmap)
+                               (filter identity)
+                               (apply concat)
+                               (sort)
+                               (partition-by identity)
+                               (map (fn [s] (if (> (count s) count-filter-level) [(first s) (count s)])))
+                               (filter identity)
+                               (sort-by last)
+                               (reverse))]
+              (f id name matches)))
+         (partition-all (/ (count nmap) (inc (.availableProcessors (Runtime/getRuntime)))) nmap))))))
 
 (comment
- (defn best-matches
-   ([name-map] (best-matches name-map 10 (/ Math/PI 2.0)))
-   ([name-map n-matches min-weight]
-      (map
-       (fn [[id matches]]
-         [(name-map id)
-          (take n-matches
-                (reverse
-                 (sort-by last
-                          (filter (fn [[k v]] (> v min-weight))
-                                  (map (fn [[k v]] [(name-map k) (/ v (count (name-map k)))])
-                                       matches)))))])
-       (co-occurring-ids-map name-map 1)))))
+  (defn best-matches
+    ([name-map] (best-matches name-map 10 (/ Math/PI 2.0)))
+    ([name-map n-matches min-weight]
+       (map
+        (fn [[id matches]]
+          [(name-map id)
+           (take n-matches
+                 (reverse
+                  (sort-by last
+                           (filter (fn [[k v]] (> v min-weight))
+                                   (map (fn [[k v]] [(name-map k) (/ v (count (name-map k)))])
+                                        matches)))))])
+        (co-occurring-ids-map name-map 1)))))
 
 (defn -main []
-(do
- (time
-  (compare-names (fn [id name matches]
-                   (do (spit (str id ".match") (-> matches (vec) (str)))))
-                 (names-map)))
- (shutdown-agents)))
+  (do
+    (time
+     (compare-names (fn [id name matches]
+                      (do (spit (str "output/" id ".match") (-> matches (vec) (str)))))
+                    (names-map)))
+    (shutdown-agents)))
