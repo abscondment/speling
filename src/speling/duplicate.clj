9d8693f7ff6a82089a327ee23cb0627a22f92850
abscondment <brendan.ribera@gmail.com>
2012-07-08 08:10:12 -0700

    Refactor to allow comparison for a single name/entity.

--- src/speling/duplicate.clj
+++ src/speling/duplicate.clj
@@ -16,51 +16,59 @@
       (transient {})
       name-map))))
 
-(defn- filtered-name-frequencies
-  ([name-freqs] (filtered-name-frequencies name-freqs 1))
-  ([name-freqs min-limit]
-     (delete-if name-freqs (fn [[k v]] (> (count v) min-limit)))))
+(defn- filtered-name-frequencies [name-freqs opts]
+  (let [min-limit (get opts :freq-filter-level 3)]
+    (delete-if name-freqs (fn [[k v]] (> (count v) min-limit)))))
+
+(defn names-map-to-frequencies-map [name-map opts]
+  (-> name-map
+      (name-frequencies opts)
+      (filtered-name-frequencies opts)))
+
+(defn compute-matches [name nmap fnmap opts]
+  (let [weight-filter-level (get opts :weight-filter-level 1)
+        min-ngram (get opts :min-ngram 3)
+        max-ngram (get opts :max-ngram 7)
+        name-weight (count name)]
+    (->> (ngrams name min-ngram max-ngram)
+         (map
+          (fn [ngram]
+            (for [id (fnmap ngram)]
+              [id (count ngram)])))
+         (filter not-empty)
+         (apply concat)
+         (sort-by first)
+         (partition-by first)
+         (map
+          (fn [pairs]
+            [(ffirst pairs)
+             ;; TODO:
+             ;; Right now, we're scaling ngram *size* by the name size.
+             ;;
+             ;; We should also test using TF*IDF for the given ngram.
+             (->> pairs (map last) (reduce +) ((fn [w] (/ w name-weight))))]))
+         (filter
+          (fn [[id weight]]
+            (> weight weight-filter-level))))))
 
 (defn compare-names
   ([nmap f] (compare-names nmap f {}))
-  ([nmap f opts]
-     (let [weight-filter-level (get opts :weight-filter-level 1)
-           freq-filter-level  (get opts :freq-filter-level 3)
-           min-ngram (get opts :min-ngram 3)
-           max-ngram (get opts :max-ngram 7)
-           n-threads (get opts :n-threads
+  ([nmap f opts] (compare-names
+                  nmap
+                  (-> nmap
+                      (name-frequencies opts)
+                      (filtered-name-frequencies opts))
+                  f
+                  opts))
+  ([nmap fnmap f opts]
+     (let [n-threads (get opts :n-threads
                           (/ (count nmap)
-                             (inc (.availableProcessors (Runtime/getRuntime)))))          
-           idfn (comp nmap first)
-           fnmap (-> nmap
-                     (name-frequencies opts)
-                     (filtered-name-frequencies freq-filter-level))]
+                             (inc (.availableProcessors (Runtime/getRuntime)))))]
        (doall
         (pmap
          (fn [groups]
            (doseq [[id name] groups]
-             (let [name-weight (count name)
-                   matches (->> (ngrams name min-ngram max-ngram)
-                                (map
-                                 (fn [ngram]
-                                   (for [id (fnmap ngram)]
-                                     [id (count ngram)])))
-                                (filter not-empty)
-                                (apply concat)
-                                (sort-by first)
-                                (partition-by first)
-                                (map
-                                 (fn [pairs]
-                                   [(ffirst pairs)
-                                    ;; TODO:
-                                    ;; Right now, we're scaling ngram *size* by the name size.
-                                    ;;
-                                    ;; We should also test using TF*IDF for the given ngram.
-                                    (->> pairs (map last) (reduce +) ((fn [w] (/ w name-weight))))]))
-                                (filter
-                                 (fn [[id weight]]
-                                   (> weight weight-filter-level))))]
-               (f id name matches))))
+             (f id name (compute-matches name nmap fnmap opts))))
          (partition-all n-threads nmap)))
        nil)))
 
