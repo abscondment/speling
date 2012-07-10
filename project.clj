979f5dfd60795974334db0f3edbb462226c58dd8
Brendan Ribera <brendan.ribera@gmail.com>
2012-07-09 17:01:24 -0700

    Bump versions. Reorder.

--- project.clj
+++ project.clj
@@ -1,10 +1,14 @@
 (defproject speling "0.5.0-SNAPSHOT"
   :description "Got speling?"
   :dependencies [[org.clojure/clojure "1.4.0"]
+
+                 ;; data access
                  [org.clojure/java.jdbc "0.2.3"]
                  [mysql/mysql-connector-java "5.1.20"]
 
-                 [ring/ring-jetty-adapter "1.1.1"]
-                 [ring-json-params "0.1.3"]
+                 ;; web tier
                  [compojure "1.1.0"]
-                 [cheshire "4.0.0"]])
+                 [cheshire "4.0.0"]
+                 [ring/ring-jetty-adapter "1.1.1"]
+                 [fuziontech/ring-json-params "0.2.0"]])
+
